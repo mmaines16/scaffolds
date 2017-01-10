c2b15f38b8a2099721b6fd823e5885145905d0ec
Mason Maines (SunGard-Lake Mary) <mason.maines@sungardps.com>
2017-01-10 13:42:59 -0500

    initial commit

---
+++ react/src/js/client.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+class ComponentName extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Welcome to React!</h1>
+      </div>
+      );
+    }
+}
